5be10fcaa65aa88fb20c2031e5fc909c83ac1d7c
Administrator <admin@example.com>
2021-04-26 14:54:07 +0800

    登录注册

---
+++ app/src/main/java/com/rifeng/p2p/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.rifeng.p2p.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rifeng.p2p.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
