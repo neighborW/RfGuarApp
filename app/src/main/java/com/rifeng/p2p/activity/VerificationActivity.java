e0b18ec8235ff1d31fe9d4348ba8a5af21cdab21
Administrator <admin@example.com>
2021-04-27 08:59:06 +0800

    验证，找回密码

---
+++ app/src/main/java/com/rifeng/p2p/activity/VerificationActivity.java
@@ -0,0 +1,15 @@
+package com.rifeng.p2p.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rifeng.p2p.R;
+
+public class VerificationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
