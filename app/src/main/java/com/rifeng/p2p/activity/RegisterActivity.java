9ec9581badbfcb97d47dbff986e7ff766a157fd8
Administrator <admin@example.com>
2021-04-27 08:29:32 +0800

    注册

--- app/src/main/java/com/rifeng/p2p/activity/RegisterActivity.java
+++ app/src/main/java/com/rifeng/p2p/activity/RegisterActivity.java
@@ -1,16 +1,51 @@
 package com.rifeng.p2p.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import com.rifeng.p2p.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends BaseActivity {
 
+    private ImageView ivBack;
+    private EditText fistName;
+    private EditText lastName;
+    private EditText agenName;
+    private EditText postCode;
+    private EditText contactName;
+    private EditText area;
+    private EditText address;
+
+    private Button summit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initComponent();
+        initEvent();
+    }
+    @SuppressLint("WrongViewCast")
+    public void initComponent(){
+        ivBack = findViewById(R.id.im_back);
+        summit = findViewById(R.id.sumit);
+        fistName = findViewById(R.id.frstName);
+        lastName = findViewById(R.id.lastName);
+        agenName = findViewById(R.id.agentName);
+        postCode = findViewById(R.id.et_postCode);
+        contactName = findViewById(R.id.contactNumber);
+        area = findViewById(R.id.et_area);
+        address = findViewById(R.id.et_address);
+    }
+    public void initEvent(){
+        //点退出按钮回到登录界面
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateTo(LoginActivity.class);
+            }
+        });
     }
 }
