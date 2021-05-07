9fa7fbd5116f0c5d0cd6529c7120463bbfd7ca31
Administrator <admin@example.com>
2021-05-07 14:48:44 +0800

    signUp

---
+++ app/src/main/java/com/rifeng/p2p/activity/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.rifeng.p2p.activity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rifeng.p2p.R;
+public class SignupActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+}
