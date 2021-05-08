2e763714dc443b96ac15091ec299433229656cf9
Administrator <admin@example.com>
2021-05-08 08:24:17 +0800

    邮箱验证

---
+++ app/src/main/java/com/rifeng/p2p/activity/VerifyEmailActivity.java
@@ -0,0 +1,12 @@
+package com.rifeng.p2p.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.rifeng.p2p.R;
+public class VerifyEmailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_email);
+    }
+}
